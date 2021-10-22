93e9c0c4f6f460ec20b3cc642231519714cf948f
praiseopara <cpopara2010@yahoo.com>
2021-10-22 12:06:58 -0500

    update

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,7 +5,7 @@ function Footer() {
     return (
         <div className="footer">
             <h1 className="center">
-                <a href="linkedin.com/in/chigoziri-praise-opara">
+                <a href="https://www.linkedin.com/in/chigoziri-praise-opara/">
                     <i className="fab fa-linkedin" />
                 </a>
                 <a href="https://github.com/kingopara">
